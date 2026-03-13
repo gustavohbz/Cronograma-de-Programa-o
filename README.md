2ec8b9b39eada69276198b46d06a100850a94cb6
Gustavo Henrique <gustavosantosgmn@gmail.com>
2026-03-12 22:45:44 -0300

    Etapa 1 Concluída + Planejamento Att
    Etapa 1 concluída com a construção do Index(HTML),Estilo(CSS) e Script(JS)
    Planejamento do projeto e definição de Deadlines para cada etapa

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Cronograma de Programação
 Um cronograma para aprendizagem de linguagens de programação,em que o usuário escolhe as liguagens e cria-se um cronograma para a semana separando por horários,a quantidade de aulas e fazendo uma distribuição dessas aulas de maneira igual para serem vistas 
+
+
+Etapas:
+- Etapa 1: Esqueleto(concluído)
+Criar o esqueleto(HTML,CSS e JS)
+
+- Etapa 2 : Cálculos Essenciais(12/03/26 a 15/03/26 )
+criar as funções que farão os cálculos e armazenamento de dados do input
+
+- Etapa 3 : Estilização CSS(15/03/26 a 20/03/26)
+
+- Etapa 4 : Implementação de Features(22/03/26 - 22/04/26)
+implementação de features como:
+Adição de cursos com carga horaria;
+tempo ate concluir curso com  data precisa se seguido o cronograma corretamente;
+Cálculo de Horas totais estudadas de cada matéria;
+Janelas de Alerta para:
+Muitas horas disponíveis no dia(+5h)
+Usuário coloca a opção de curso no cronograma e substitui as materias,ou adiciona.
+Dicas: Hora de praticar no cronograma junto com as aulas,exemplo
+se um bloco de 1h,mas ele tem 3 blocos ao dia,tirar 25% so para Codewars ou Prática de Debugging
--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
-let 
+// Variáveis para armazenar as horas disponíveis para cada dia da semana
+//com o .value eu capturo o valor inserido pelo usuario no input, mas como o script é carregado antes do usuario inserir os valores, ele captura o valor vazio, por isso a necessidade de criar a função ArmazenarHorasSemanas() para capturar os valores após o usuario clicar no botão "Armazenar Horas por Semana"
+var Segunda = document.getElementById("seg").value
+var Terca  = document.getElementById("ter").value
+var Quarta = document.getElementById("qua").value
+var Quinta = document.getElementById("qui").value
+var Sexta  = document.getElementById("sex").value
+var Sabado = document.getElementById("sab").value
+var Domingo = document.getElementById("dom").value
+
+function ArmazenarHorasSemanas(){
+   var Segunda = document.getElementById("seg")
+    var Terca  = document.getElementById("ter")
+    var Quarta = document.getElementById("qua")
+    var Quinta = document.getElementById("qui")
+    var Sexta  = document.getElementById("sex")
+    var Sabado = document.getElementById("sab")
+    var Domingo = document.getElementById("dom")
+    window.alert(`Só confirmando: Segunda-Feira:${Segunda.value}, Terça-Feira:${Terca.value}, Quarta-Feira:${Quarta.value}, Quinta-Feira:${Quinta.value}, Sexta-Feira:${Sexta.value}, Sábado:${Sabado.value}, Domingo:${Domingo.value}`)
+    
+}
+///window.alert(Segunda + Terca + Quarta + Quinta + Sexta + Sabado + Domingo)
+function gerarCronograma(){
+    window.alert("Gerando Cronograma...")
+}
